77a9fb7ef422f336e3fc8f68f8628ff3b58cb1eb
Binaya1997 <patnaikb450@gmail.com>
2026-08-11 06:55:30 +0530

    empty commit

--- js/script.js
+++ js/script.js
@@ -359,7 +359,7 @@ document.addEventListener("DOMContentLoaded", function () {
         }
 
         if (q.includes("contact") || q.includes("phone") || q.includes("email")) {
-            return "You can contact PeopleJuris at +91 8249304020 or 943787008, or email swadhin@legaljuris.com.";
+            return "You can contact PeopleJuris at +91 8249304020 or 943787008, or email info@peoplejuris.com.";
         }
 
         if (q.includes("address") || q.includes("location") || q.includes("office") || q.includes("located") || q.includes("where is peoplejuris") || q.includes("where are you located") || q.includes("office") || q.includes("rayagada")  ) {
